7d6dae129d2b0c414aa8466e5efdf946b02980c9
eunicelok <eunicelp511@gmail.com>
2020-03-10 01:38:21 +0800

    Tidy up index.html.erb and added 'back' at archive.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,13 @@
 
 # SELECT * FROM boards WHERE created_at BETWEEN '2020-03-08' AND '2020-03-09';
 
-  user1 = User.new(email: 'nick@gmail.com', encrypted_password: '123456', created_at: 'now', updated_at: 'now' );
-  user1.save
+  # user1 = User.new(email: 'nick@gmail.com', encrypted_password: '123456', created_at: 'now', updated_at: 'now' );
+  # user1.save
 
-  user2 = User.new(email: 'may@gmail.com', encrypted_password: '234567', created_at: 'now', updated_at: 'now' );
-  user2.save
+  # user2 = User.new(email: 'may@gmail.com', encrypted_password: '234567', created_at: 'now', updated_at: 'now' );
+  # user2.save
 
+# YET TO TEST SEED FILE
   board1 = Board.new(accomplishment: 'did Ruby', challenges: 'fiddle with React', task: 'complete project 4', resources: 'stack overflow', user_id: 2);
   board1.save
 
