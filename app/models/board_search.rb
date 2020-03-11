4fe57249163970339da31c78e5fcd45bdfa5c15e
eunicelok <eunicelp511@gmail.com>
2020-03-11 08:50:20 +0800

    Positioned for add/edit form

--- app/models/board_search.rb
+++ app/models/board_search.rb
@@ -5,7 +5,7 @@ class BoardSearch
     # the below code indicates that we're going to be adding items into the params
     params ||= {}
     # grabbing the params from date_from, the 7.days.ago is a Ruby method, taking the date 7 days ago from today's date
-    @date_from = parsed_date(params[:date_from], 7.days.ago.to_date.to_s)
+    @date_from = parsed_date(params[:date_from], 2.days.ago.to_date.to_s)
     # grabbing the params from date_to, takes in the ruby class Date, today means automatically taking in today's class
     @date_to = parsed_date(params[:date_to], Date.today.to_s)
   end
